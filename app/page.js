08deeaf23d5cceaff9094427d7bd29c7f9a5fb41
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-24 18:23:51 +0100

    updated page.jsx

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+import BreakingPrices from "@components/BreakingPrices";
 import Cloud from "@components/Cloud";
+import DemandAccess from "@components/DemandAccess";
 import Hero from "@components/Hero";
+import MoreGPUs from "@components/MoreGPUs";
+import Partners from "@components/Partners";
 
 const Home = () => {
 	return (
 		<div>
 			<Hero />
 			<Cloud />
+			<DemandAccess />
+			<BreakingPrices />
+			<MoreGPUs />
+			<Partners />
 		</div>
 	);
 };
