3943ab6204fece5c47be672df0bdb6207399c957
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-25 07:55:09 +0100

    added id ventures component

---
+++ components/IdVentures.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const IdVentures = () => {
+	return (
+		<div className="flex justify-center items-center mt-8">
+			<Image
+				src="https://io.net/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fmission-bg-tablet.4cfd5a7f.png&w=3840&q=75"
+				width={1000}
+				height={300}
+			/>
+		</div>
+	);
+};
+
+export default IdVentures;
