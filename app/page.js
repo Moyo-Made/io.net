1b310cedebaeacc2202727a7c6306fba46cf0848
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-07-25 09:12:14 +0100

    updated home page route

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
+import About from "@components/About";
+import Animation from "@components/Animation";
 import BreakingPrices from "@components/BreakingPrices";
 import Cloud from "@components/Cloud";
 import DemandAccess from "@components/DemandAccess";
+import Explore from "@components/Explorer";
 import Hero from "@components/Hero";
+import Infrastructure from "@components/Infrastructure";
 import MoreGPUs from "@components/MoreGPUs";
 import Partners from "@components/Partners";
+import Profits from "@components/Profits";
+import Work from "@components/Work";
 
 const Home = () => {
 	return (
@@ -14,6 +20,12 @@ const Home = () => {
 			<BreakingPrices />
 			<MoreGPUs />
 			<Partners />
+			<Work />
+			<Profits />
+			<Infrastructure />
+			<Explore />
+			<About />
+			<Animation />
 		</div>
 	);
 };
